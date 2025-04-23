4050560629eb759f2a0280712245f1a01ca96048
KangbingZhao <447953883@qq.com>
2025-04-23 14:38:06 +0800

    fix domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const repoName = 'personal_homepage'; // 替换为你的仓库名
+
 const nextConfig = {
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   trailingSlash: true,
 };
 
